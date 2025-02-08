3de96d3f271cd631f23f5e416e62eee0bf7225cb
Pratikkuntawar <137525410+Pratikkuntawar@users.noreply.github.com>
2025-02-08 11:55:12 +0530

    Update EditorPage.js

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -96,11 +96,12 @@ const EditorPage = () => {
             <div className="aside">
                 <div className="asideInner">
                     <div className="logo">
-                        <img
-                            className="logoImage"
-                            src="/code-sync.png"
-                            alt="logo"
-                        />
+                        // <img
+                        //     className="logoImage"
+                        //     src="/code-sync.png"
+                        //     alt="logo"
+                        // />
+         <h1>Welcome To Code Connect</h1>
                     </div>
                     <h3>Connected</h3>
                     <div className="clientsList">
